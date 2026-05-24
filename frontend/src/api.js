0814988c7f2878b4b80aa927adbe04aa2d134c54
ebrahemmoh00-arch <ebrahemmoh00@gmail.com>
2026-05-24 10:36:08 +0300

    Rename API_URL to API_BASE for clarity

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://cmms-system.render.com";
+const API_BASE = "https://cmms-system.render.com";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
